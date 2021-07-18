11e540d24032e453c6e3e8c99f20fe45f97274db
ngocnhan272812 <86595292+ngocnhan272812@users.noreply.github.com>
2021-07-18 09:12:59 +0700

    Update service_client.cpp

--- service_client.cpp
+++ service_client.cpp
@@ -1,29 +1,33 @@
 #include "ros/ros.h"
-#include "<package_name>/<srv_name>.h"
+#include "<package_name>/<srv_file_name>.h"
 #include <cstdlib>
 
 int main(int argc, char **argv)
 {
-        //Initialize node name "<give a node name>"
+        /** Initialize node name "<give a node name>" **/
         ros::init(argc, argv, "<node name>");
 
         ros::NodeHandle n;
-        ros::ServiceClient client = n.serviceClient<package_name::srv_name>("geometry_type_move");
-        turtlesim_tutorial::geometry_type  srv;
+        ros::ServiceClient client = n.serviceClient<package_name::srv_file_name>("<service name>");
+        package_name>::<srv_flie_name>.h  srv;
 
-        // Input requests 
-        srv.request.speed = 2;
-        srv.request.clockwise = 0;
-
+        /**** Input requests **** 
+        rv.request.service1 = 2;
+        srv.request.service2 = 0;
+        ......    
+        */
+        
+        /**** call service ****
         if (client.call(srv))
         {
-                ROS_INFO(" Respone: %s ", srv.response.name.c_str() );
+                ROS_INFO(" Respone: %s ", srv.response.name.c_str() );    //check response
         }
         else
         {
                 ROS_ERROR("Failed to call sever geometry_type_move");
                 return 1;
         }
+        */
 
         return 0;
 
